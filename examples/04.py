4218157da13ebf91484df5636b557c8c4288994c
Андрей Беляков <ttxiom@gmail.com>
2024-12-11 19:34:43 +0500

    up

---
+++ examples/04.py
@@ -0,0 +1,7 @@
+x = 100
+
+i = 1  # index
+while i <= 5:
+    print(i, x*i)
+    i += 1  # i = i + 1
+pass
---
+++ examples/05.py
@@ -0,0 +1,27 @@
+x = 100
+
+for i in range(5, 0, -1):  # 5 4 3 2 1
+    print(i, x*i)
+
+# for i in range(5):  # 0 1 2 3 4
+#     d = i+1
+#     print(d, x*d)
+
+# for i in range(1, 6):
+#     print(i, x*i)
+
+# for i in range(1, 6, 1):
+#     print(i, x*i)
+
+# for i in 1,2,3,4,5:
+#     print(i, x*i)
+
+"""
+i = 1  # index
+while i <= 5:
+    print(i, x*i)
+    i += 1  # i = i + 1
+
+    
+pass
+"""
